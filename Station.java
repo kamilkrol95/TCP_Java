83704202cd41f20eb682b8f2d493e37ab2ccfbc2
kamilkrol95 <kamilkrol95@gmail.com>
2019-05-10 20:28:21 +0200

    Add station state

--- Station.java
+++ Station.java
@@ -1,9 +1,35 @@
 package udp;
 
-public class Station {
+import java.io.Serializable;
+
+enum State {
+    WORK, PASS, FAILURE, DEFAULT
+}
+
+public class Station implements Serializable {
 
     private Integer stationID;
     private String description;
+    private State state = State.DEFAULT;
+
+    @Override
+    public String toString(){
+        return "[STATION ID: " + stationID + "]-> [" + description + "], State: [" + state.toString() + "]";
+    }
+
+    public Station() {
+        this.stationID = 2;
+        this.description = "Drilling";
+    }
+
+    public Station(Integer stationID) {
+        this.stationID = stationID;
+    }
+
+    public Station(Integer stationID, String description) {
+        this.stationID = stationID;
+        this.description = description;
+    }
 
     public String getDescription() {
         return description;
@@ -11,6 +37,10 @@ public class Station {
 
     public Integer getStationID() {
         return stationID;
+    }
+
+    public State getState() {
+        return state;
     }
 
     public void setDescription(String description) {
@@ -21,13 +51,7 @@ public class Station {
         this.stationID = stationID;
     }
 
-    public Station() {
-        this.stationID = 2;
-        this.description = "Drilling";
-    }
-
-    public Station(Integer stationID, String description) {
-        this.stationID = stationID;
-        this.description = description;
+    public void setState(State state) {
+        this.state = state;
     }
 }
